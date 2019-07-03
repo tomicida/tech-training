51684d6e56957e8e2c9488ad68a61483ab209ce3
Thomas Almeida <nethomas10@hotmail.com>
2019-07-03 10:31:37 +0100

    initialized weather component

---
+++ react/tutorial/src/Weather.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class Weather extends Component{
+	constructor(props){
+		this.state = {};	
+	}
+
+	componentDidMount(){
+		const url = ""; //api.openweathermap.org/data/2.5/forecast?id=
+		fetch(url).then();
+	}
+
+	render(){
+		return (
+		
+		);	
+	}
+}
